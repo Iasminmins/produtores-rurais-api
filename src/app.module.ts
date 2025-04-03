38956f574197649151216b3c344affdc337b8f1c
Iasmin Oliveira <160276015+Iasminmins@users.noreply.github.com>
2025-04-03 16:19:19 -0300

    Atualizar o app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProducerModule } from './producer/producer.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'api-rurap',
+      password: process.env.DB_PASSWORD || '81204413',
+      database: process.env.DB_DATABASE || 'produtores',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, 
+    }),
+    ProducerModule,
+  ],
 })
 export class AppModule {}
